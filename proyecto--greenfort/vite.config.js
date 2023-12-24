61eb6c98a665115f115234a7862c3121e3618f5d
Bayron Silva <bayronisraelsilva@gmail.com>
2023-12-24 00:55:44 -0300

    correccion

--- proyecto--greenfort/vite.config.js
+++ proyecto--greenfort/vite.config.js
@@ -1,7 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/preentrega-silva/', // Cambia 'tu-repositorio' al nombre de tu repositorio en GitHub
 })
